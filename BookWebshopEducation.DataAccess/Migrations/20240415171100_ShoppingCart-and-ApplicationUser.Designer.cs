a137b3a118a1d2916a67cb8bf87a1f7b94ac0e62
TihoAska <tihomirascic18@gmail.com>
2024-04-23 12:50:54 +0200

    New migrations, package update.

--- BookWebshopEducation.DataAccess/Migrations/20240415171100_ShoppingCart-and-ApplicationUser.Designer.cs
+++ BookWebshopEducation.DataAccess/Migrations/20240415171100_ShoppingCart-and-ApplicationUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BookWebshopEducation.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BookWebshopEducation.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240415171100_ShoppingCart-and-ApplicationUser")]
+    partial class ShoppingCartandApplicationUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
