24d8343e73464d68f5775a530c27f7385fd10eb9
TihoAska <tihomirascic18@gmail.com>
2024-04-24 19:27:26 +0200

    Added Company to ApplicationUser.

--- BookWebshopEducation.Models/Models/ApplicationUser.cs
+++ BookWebshopEducation.Models/Models/ApplicationUser.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,5 +18,8 @@ namespace BookWebshopEducation.Models.Models
         public string PostalCode { get; set; } = string.Empty;
         public string Country { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
+        public int? CompanyId { get; set; }
+        [ForeignKey("CompanyId")]
+        public Company Company { get; set; }
     }
 }
