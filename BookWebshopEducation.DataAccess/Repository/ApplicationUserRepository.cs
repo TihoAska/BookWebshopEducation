98c10bcad5309af2085ecd305327ec977d489112
TihoAska <tihomirascic18@gmail.com>
2024-04-24 19:28:49 +0200

    Added ShoppingCart and ApplicationUser Repositories.

---
+++ BookWebshopEducation.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,25 @@
+﻿using BookWebshopEducation.DataAccess.Data;
+using BookWebshopEducation.DataAccess.Repository.IRepository;
+using BookWebshopEducation.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWebshopEducation.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        ApplicationDbContext _context;
+        public ApplicationUserRepository(ApplicationDbContext context) : base(context)
+        {
+               _context = context;
+        }
+
+        public void Update(ApplicationUser applicationUser)
+        {
+            _context.Update(applicationUser);
+        }
+    }
+}
---
+++ BookWebshopEducation.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,24 @@
+﻿using BookWebshopEducation.DataAccess.Data;
+using BookWebshopEducation.DataAccess.Repository.IRepository;
+using BookWebshopEducation.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWebshopEducation.DataAccess.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private ApplicationDbContext _context;
+        public ShoppingCartRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+        public void Update(ShoppingCart shoppingCart)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
